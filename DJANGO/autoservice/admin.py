d8d355bd5037be3f122d38fd93c1af3cd4a7bc03
EldarasPetrokas <elpetrokas@gmail.com>
2024-11-28 15:11:58 +0200

    Update DJANGO project with new functionality

--- DJANGO/autoservice/admin.py
+++ DJANGO/autoservice/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from .models import AutomobilioModelis, Automobilis, Paslauga, Uzsakymas, UzsakymoEilute
 
+class UzsakymoEiluteInline(admin.TabularInline):
+    model = UzsakymoEilute
+    extra = 1
+
+class UzsakymasAdmin(admin.ModelAdmin):
+    list_display = ('id', 'automobilis', 'data')
+    inlines = [UzsakymoEiluteInline]
+
+class AutomobilisAdmin(admin.ModelAdmin):
+    list_display = ('klientas', 'automobilio_modelis', 'valstybinis_nr', 'vin_kodas')
+    list_filter = ('klientas', 'automobilio_modelis')
+    search_fields = ('valstybinis_nr', 'vin_kodas')
+
+class PaslaugaAdmin(admin.ModelAdmin):
+    list_display = ('pavadinimas', 'kaina')
+
+
 admin.site.register(AutomobilioModelis)
-admin.site.register(Automobilis)
-admin.site.register(Paslauga)
-admin.site.register(Uzsakymas)
+admin.site.register(Automobilis, AutomobilisAdmin)
+admin.site.register(Paslauga, PaslaugaAdmin)
+admin.site.register(Uzsakymas, UzsakymasAdmin)
 admin.site.register(UzsakymoEilute)
